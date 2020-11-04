2def81cfde6000360daab50a11370d7a9c88133c
Samuel S. Siegmeister <siegmeister.s@husky.neu.edu>
2020-11-04 15:55:41 -0500

    colors

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Account } from '../account';
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+})
 
 @Component({
   selector: 'account-detail',
@@ -9,9 +16,39 @@ export class AccountComponent implements OnInit {
 
   @Input() account: Account;
 
+  modifier: string = "";
+
+  acctNum(): string {
+    let num: string = this.account.num.toString();
+    while (num.length < 4) num = "0" + num;
+    return num;
+  }
+
+  acctBal(): string {
+    return formatter.format(this.account.balance);
+  }
+
+  acctChangeNum(): string {
+    return formatter.format(Math.abs(this.account.change.numeric));
+  }
+
+  acctChangePercent(): string {
+    return this.modifier + Math.abs(this.account.change.percent).toFixed(2) + '%';
+  }
+
+  balanceModifier(): void {
+    if (this.account.change.percent > 0) {
+      this.modifier = "+";
+    } else if (this.account.change.percent < 0) {
+      this.modifier = "-";
+    }
+  }
+
+
   constructor() { }
 
   ngOnInit(): void {
+    this.balanceModifier();
   }
 
 }
